6ad01eff7256eb9a4d0036867b71ef7cd679e2a1
Vladimir <tune@artskills.ru>
2017-08-22 13:10:12 +0300

    Add reaction actions

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Zabbix to Slack channel alert notificator
 It is a simple PHP script that uses the custom alert script functionality within [Zabbix](https://www.zabbix.com/documentation/3.0/manual/config/notifications/media/script) along with the Slack [Web Api](https://api.slack.com/methods) 
 
+## Requirements
+* PHP greater than 5.4.
+* Default zabbix notification messages. 
+
+## Features
+* Message threading by event ID.
+* Icons by event priority.
+* Add reaction icon for resolved alerts.
+
 ## Installation
 * Download project
 * Make symbolic link to ``bin/sendSlackAlert.php`` to your ``AlertScriptsPath`` directory (that is specified within the Zabbix servers' configuration file ``zabbix_server.conf``).
@@ -11,6 +20,7 @@ It is a simple PHP script that uses the custom alert script functionality within
 	* Invite bot for private channels.
 	* Place "OAuth Access Token" to ``SLACK_ACCESS_TOKEN`` constant.
 	* Fill ``SLACK_USER_NAME`` constant.
+	* Fill ``ZABBIX_FRONTEND_URL`` constant.
 * Add new media type: Administration -> Create media type:
 	* Name: ``Slack``
 	* Type: ``Script``
--- src/SlackAlert.php
+++ src/SlackAlert.php
@@ -7,6 +7,26 @@ class SlackAlert
 	const SLACK_URL = 'https://slack.com/api/';
 	const SLACK_SUCCESS_RESULT = 'ok';
 
+	const SEARCH_EVENT_PATTERN = '/Original event ID\:\s([0-9]+)\s?/m';
+	const SUCCESS_MESSAGE_PREFIX = 'OK:';
+
+	const ICON_LIST = [
+		'Default' => ':information_source:',
+		'Information' => ':information_source:',
+		'Warning' => ':warning:',
+		'Average' => ':exclamation:',
+		'High' => ':bangbang:',
+		'Disaster' => ':no_entry:',
+		'OK' => ':shamrock:',
+	];
+
+	const DIR_ACTIVE = 'active';
+	const DIR_RESOLVED = 'resolved';
+
+	const SUCCESS_REACTION_ICON = 'white_check_mark';
+
+	const CLEAN_SUCCESS_MESSAGES_TIMEOUT = '-1 day';
+
 	/**
 	 * Токен
 	 *
@@ -31,6 +51,14 @@ class SlackAlert
 	{
 		$this->_slackToken = $slackToken;
 		$this->_slackUserName = $slackUserName;
+
+		if (!is_dir(ROOT . '/tmp/' . static::DIR_ACTIVE)) {
+			mkdir(ROOT . '/tmp/' . static::DIR_ACTIVE);
+		}
+
+		if (!is_dir(ROOT . '/tmp/' . static::DIR_RESOLVED)) {
+			mkdir(ROOT . '/tmp/' . static::DIR_RESOLVED);
+		}
 	}
 
 	/**
@@ -48,13 +76,14 @@ class SlackAlert
 			'channel' => $channel,
 			'as_user' => false,
 			'username' => $this->_slackUserName,
-			'text' => $subject . "\n" . $message,
+			'text' => '*' . $subject . "*\n" . preg_replace(static::SEARCH_EVENT_PATTERN, 'Original event ID: <' . ZABBIX_FRONTEND_URL . '|$1>', $message),
+			'icon_emoji' => $this->_detectMessageIcon($subject, $message),
 		];
 
 		$evenId = $this->_getEventId($message);
 		if ($evenId !== null) {
 			$workDir = ROOT . '/tmp';
-			$eventFile = $workDir . '/' . $evenId . '.json';
+			$eventFile = $workDir . '/' . static::DIR_ACTIVE . '/' . $evenId . '.json';
 			if (is_file($eventFile) && ($threadPost = json_decode(file_get_contents($eventFile), true))) {
 				$newThread = false;
 				$sendData['thread_ts'] = $threadPost['ts'];
@@ -69,12 +98,35 @@ class SlackAlert
 			if ($newThread) {
 				file_put_contents($eventFile, json_encode($result));
 			} else {
-				if (substr($subject, 0, 3) === 'OK:' && is_file($eventFile)) {
-					unlink($eventFile);
+				if ($this->_isSuccessMessage($subject) && is_file($eventFile)) {
+					$this->_addSuccessReaction($threadPost);
+					$this->_clearOldMessages();
+					$resolvedFile = $workDir . '/' . static::DIR_RESOLVED . '/' . $evenId . '.json';
+					if (is_file($resolvedFile)) {
+						unlink($resolvedFile);
+					}
+					rename($eventFile, $resolvedFile);
 				}
 			}
 		}
-		return true;
+		return $result[static::SLACK_SUCCESS_RESULT];
+	}
+
+	/**
+	 * Добавляем пометку о том, что проблема решена
+	 *
+	 * @param array $threadPost
+	 * @return bool
+	 */
+	private function _addSuccessReaction($threadPost)
+	{
+		$result = $this->_sendData('reactions.add', [
+			'channel' => $threadPost['channel'],
+			'timestamp' => $threadPost['ts'],
+			'name' => static::SUCCESS_REACTION_ICON,
+		]);
+
+		return $result[static::SLACK_SUCCESS_RESULT];
 	}
 
 	/**
@@ -85,7 +137,7 @@ class SlackAlert
 	 */
 	private function _getEventId($message)
 	{
-		if (preg_match('/Original event ID:\s([0-9]+)\s?/m', $message, $matches)) {
+		if (preg_match(static::SEARCH_EVENT_PATTERN, $message, $matches)) {
 			return $matches[1];
 		} else {
 			return null;
@@ -104,5 +156,54 @@ class SlackAlert
 		$data['token'] = $this->_slackToken;
 		$result = file_get_contents(static::SLACK_URL . $method . '?' . http_build_query($data));
 		return json_decode($result, true);
+	}
+
+	/**
+	 * Определяем иконку сообщения
+	 *
+	 * @param string $subject
+	 * @param string $message
+	 * @return string
+	 */
+	private function _detectMessageIcon($subject, $message)
+	{
+		if ($this->_isSuccessMessage($subject)) {
+			return static::ICON_LIST['OK'];
+		} else {
+			if (preg_match('/Trigger severity\:\s([A-Za-z]+)\s/', $message, $matches)) {
+				if (array_key_exists($matches[1], static::ICON_LIST)) {
+					return static::ICON_LIST[$matches[1]];
+				}
+			}
+			return static::ICON_LIST['Default'];
+		}
+	}
+
+	/**
+	 * Является ли данное сообщение уведомлением о решении проблемы
+	 *
+	 * @param string $subject
+	 * @return bool
+	 */
+	private function _isSuccessMessage($subject)
+	{
+		return substr($subject, 0, 3) === static::SUCCESS_MESSAGE_PREFIX;
+	}
+
+	/**
+	 * Чистим старые успешные уведомления
+	 */
+	private function _clearOldMessages()
+	{
+		$dirPath = ROOT . '/tmp/' . static::DIR_RESOLVED;
+		$workDir = dir($dirPath);
+		$timeout = strtotime(self::CLEAN_SUCCESS_MESSAGES_TIMEOUT);
+		while (false !== ($entry = $workDir->read())) {
+			$workFile = $dirPath . '/' . $entry;
+			if (is_file($workFile) && filemtime($workFile) <= $timeout) {
+				unlink($workFile);
+			}
+		}
+		$workDir->close();
 	}
 }
